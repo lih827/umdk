5415f56fdccf7f84b63c98658dd4b625b6b46bec
lee-yuxing <liyuxing5@huawei.com>
2025-12-12 17:03:35 +0800

    cam: fix pybind normal dispatch & combine whl packet compile issue

--- src/cam/comm_operator/pybind/pybind.cpp
+++ src/cam/comm_operator/pybind/pybind.cpp
@@ -13,6 +13,8 @@
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     m.def("fused_deep_moe", &fused_deep_moe_impl_autograd, "fused_deep_moe");
+    m.def("moe_dispatch_normal", &moe_dispatch_normal_impl_autograd, "moe_dispatch_normal");
+    m.def("moe_combine_normal", &moe_combine_normal_impl_autograd, "moe_combine_normal");
 }
 
 TORCH_LIBRARY(umdk_cam_op_lib, m) {
@@ -20,4 +22,10 @@ TORCH_LIBRARY(umdk_cam_op_lib, m) {
     Tensor gmm2Weight, Tensor gmm2WeightScale, Tensor expertSmoothScalesOptional, Tensor expertScalesOptional, \
     str groupEp, int epRankSize, int epRankId, int moeExpertNum, int shareExpertNum, int shareExpertRankNum, \
     int quantMode, int globalBs) -> Tensor");
+    m.def("moe_dispatch_normal(Tensor x, Tensor topkIdx, Tensor sendOffset, Tensor sendTokenIdx, Tensor recvOffset, \
+    Tensor recvCount, str groupEp, int epWorldSize, int epRankId, str groupTp, int tpWorldSize, int tpRankId, \
+    int moeExpertNum, int quantMode, int globalBs) -> (Tensor, Tensor, Tensor)");
+    m.def("moe_combine_normal(Tensor recvX, Tensor tokenSrcInfo, Tensor epRecvCounts, Tensor recvTopkWeights, \
+    Tensor? tpRecvCounts, str epGroupName, int epWorldSize, int epRankId, str tpGroupName, int tpWorldSize, \
+    int tpRankId, int moeExpertNum, int globalBs) -> Tensor");
 }
