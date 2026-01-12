27f3b2de75164f72ff8e9864ed8207610aaef8ca
lih827 <383084552@qq.com>
2026-01-12 14:39:10 +0800

    update kernel
    torch change
    
    test code

--- src/cam/comm_operator/ascend_kernels/dispatch_layout/op_host/dispatch_layout.cpp
+++ src/cam/comm_operator/ascend_kernels/dispatch_layout/op_host/dispatch_layout.cpp
@@ -1,17 +1,8 @@
-/*
- * SPDX-License-Identifier: MIT
- * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
- * Description: dispatch layout function implementation file
- * Create: 2026-01-05
- * Note:
- * History: 2026-01-05 create dispatch layout function file
- */
-
 #include "register/op_def_registry.h"
 
 namespace ops {
 class DispatchLayout : public OpDef {
-public:
+  public:
     explicit DispatchLayout(const char *name) : OpDef(name)
     {
         this->Input("topkIdx")
@@ -24,6 +15,7 @@ public:
         this->Attr("num_ranks").Int();
         this->Attr("num_experts").Int();
         this->Attr("num_topk").Int();
+        this->Attr("local_ranksize").Int();
 
         this->Output("numTokensPerRank")
             .ParamType(REQUIRED)
@@ -40,9 +32,19 @@ public:
             .DataType({ge::DT_INT32})
             .Format({ge::FORMAT_ND})
             .UnknownShapeFormat({ge::FORMAT_ND});
+        this->Output("notifySendData")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32})
+            .Format({ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND});
+        this->Output("sendTokenIdxSmall")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32})
+            .Format({ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND});
 
-        OpAICoreConfig aicore_config;
-        aicore_config.DynamicCompileStaticFlag(true)
+        OpAICoreConfig a3_config;
+        a3_config.DynamicCompileStaticFlag(true)
             .DynamicFormatFlag(true)
             .DynamicRankSupportFlag(true)
             .DynamicShapeSupportFlag(true)
@@ -52,9 +54,21 @@ public:
             .ExtendCfgInfo("jitCompile.flag", "static_true")
             .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
 
-        this->AICore().AddConfig("ascend910_93", aicore_config);
+        OpAICoreConfig a2_config;
+        a2_config.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(true)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false)
+            .PrecisionReduceFlag(true)
+            .ExtendCfgInfo("aclnnSupport.value", "support_aclnn")
+            .ExtendCfgInfo("jitCompile.flag", "static_false")
+            .ExtendCfgInfo("multiKernelSupportDynamicGraph.value", "multi_kernel");
+
+        this->AICore().AddConfig("ascend910_93", a3_config);
+        this->AICore().AddConfig("ascend910b", a2_config);
     }
 };
 
 OP_ADD(DispatchLayout);
-}  // namespace ops
+} // namespace ops
--- src/cam/comm_operator/pybind/moe_dispatch_prefill.cpp
+++ src/cam/comm_operator/pybind/moe_dispatch_prefill.cpp
@@ -7,15 +7,14 @@
  * History: 2026-01-08 create moe_dispatch_prefill pybind extention file
  */
 
-#include <unistd.h>
-#include <hccl/hccl.h>
-#include <torch/extension.h>
-#include <torch/csrc/autograd/custom_function.h>
-#include "torch_npu/csrc/core/npu/NPUStream.h"
 #include "pytorch_npu_helper.hpp"
+#include "torch_npu/csrc/core/npu/NPUStream.h"
 #include "utils.h"
 #include <hccl/hccl.h>
 #include <iostream>
+#include <torch/csrc/autograd/custom_function.h>
+#include <torch/extension.h>
+#include <unistd.h>
 
 using torch::autograd::AutogradContext;
 using torch::autograd::Function;
@@ -23,246 +22,140 @@ using tensor_list = std::vector<at::Tensor>;
 using namespace at;
 using namespace std;
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> MoeDispatchPrefillImplNpu(
-    const at::Tensor& x,
-    const at::Tensor& topkIdx,
-    const at::Tensor& topkWeights,
-    const at::Tensor& numTokensPerRank,
-    const at::Tensor& isTokenInRank,
-    at::Tensor& numTokensPerExpert,
-    int64_t numWorstTokens,
-    c10::string_view groupEp,
-    int64_t rank,
-    int64_t numRanks)
+const uint32_t NO_SCALES = 0;
+const uint32_t DYNAMIC_SCALES = 2;
+const uint32_t EXPAND_IDX_COUNT_PER_GROUP = 3;
+
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
+MoeDispatchPrefillImplNpu(const at::Tensor &x, const at::Tensor &topk_idx, const at::Tensor &topk_weights,
+                          const at::Tensor &num_tokens_per_expert, const at::Tensor &send_token_idx_small,
+                          c10::string_view groupEp, int64_t rank, int64_t num_ranks, bool use_quant)
 {
-    std::vector<char> groupEpChrs(groupEp.begin(), groupEp.end());
-    groupEpChrs.push_back('\0');
-    char* groupEpPtr = &groupEpChrs[0];
-    at::Tensor newX = x;
+    std::vector<char> group_ep_chrs(groupEp.begin(), groupEp.end());
+    group_ep_chrs.push_back('\0');
+    char *group_ep_ptr = &group_ep_chrs[0];
+
+    // Convert topk_idx to int32 if necessary
+    at::Tensor topk_idx_int32 = topk_idx.scalar_type() == at::kInt ? topk_idx : topk_idx.to(at::kInt);
+    at::Tensor expert_ids = topk_idx_int32;
+    int64_t tp_size = 1;
+    int64_t tp_rank = 0;
+    int64_t quant_mode = use_quant ? DYNAMIC_SCALES : NO_SCALES;
 
     // Type checks
-    TORCH_BIND_ASSERT(isTokenInRank.scalar_type() == at::kBool);
-    TORCH_BIND_ASSERT(numTokensPerExpert.scalar_type() == at::kInt);
-    TORCH_BIND_ASSERT(numTokensPerRank.scalar_type() == at::kInt);
+    TORCH_BIND_ASSERT(num_tokens_per_expert.scalar_type() == at::kInt);
 
     // Shape and contiguous checks
-    TORCH_BIND_ASSERT(newX.dim() == 2 and newX.is_contiguous());
-    TORCH_BIND_ASSERT(isTokenInRank.dim() == 2 and isTokenInRank.is_contiguous());
-    TORCH_BIND_ASSERT(isTokenInRank.size(0) == newX.size(0) and isTokenInRank.size(1) == numRanks);
-    TORCH_BIND_ASSERT(numTokensPerExpert.dim() == 1 and numTokensPerExpert.is_contiguous());
-    TORCH_BIND_ASSERT(numTokensPerExpert.size(0) % numRanks == 0);
-    TORCH_BIND_ASSERT(numTokensPerRank.dim() == 1 and numTokensPerRank.is_contiguous());
-    TORCH_BIND_ASSERT(numTokensPerRank.size(0) == numRanks);
+    TORCH_BIND_ASSERT(x.dim() == 2 and x.is_contiguous());
+    TORCH_BIND_ASSERT(num_tokens_per_expert.dim() == 1 and num_tokens_per_expert.is_contiguous());
+    TORCH_BIND_ASSERT(num_tokens_per_expert.size(0) % num_ranks == 0);
 
-    auto numTokens = static_cast<int>(newX.size(0));
-    auto hidden = static_cast<int>(newX.size(1));
-    auto numExperts = static_cast<int64_t>(numTokensPerExpert.size(0));
-    auto numLocalExperts = static_cast<int>(numExperts / numRanks);
+    auto num_tokens = static_cast<int>(x.size(0));
+    auto hidden = static_cast<int>(x.size(1));
+    auto num_experts = static_cast<int64_t>(num_tokens_per_expert.size(0));
+    auto num_local_experts = static_cast<int>(num_experts / num_ranks);
 
     // Top-k checks
-    int numTopk = 0;
-    numTopk = static_cast<int>(topkIdx.size(1));
-    TORCH_BIND_ASSERT(numExperts > 0);
-    TORCH_BIND_ASSERT(topkIdx.dim() == 2 and topkIdx.is_contiguous());
-    TORCH_BIND_ASSERT(topkWeights.dim() == 2 and topkWeights.is_contiguous());
-    TORCH_BIND_ASSERT(numTokens == topkIdx.size(0));
-    TORCH_BIND_ASSERT(numTopk == topkWeights.size(1));
-    TORCH_BIND_ASSERT(topkWeights.scalar_type() == at::kFloat);
+    int num_topk = static_cast<int>(topk_idx_int32.size(1));
+    TORCH_BIND_ASSERT(num_experts > 0);
+    TORCH_BIND_ASSERT(topk_idx_int32.dim() == 2 and topk_idx_int32.is_contiguous());
+    TORCH_BIND_ASSERT(topk_weights.dim() == 2 and topk_weights.is_contiguous());
+    TORCH_BIND_ASSERT(num_tokens == topk_idx_int32.size(0));
+    TORCH_BIND_ASSERT(num_topk == topk_weights.size(1));
 
-    int sendPerGroup = 3;  // (send_to_expert_num, send_to_expert_offset, send_rank_tokens)
+    int send_per_group = EXPAND_IDX_COUNT_PER_GROUP; // (send_to_expert_num, send_to_expert_offset, send_rank_tokens)
 
-    auto sendData = at::empty({numExperts * sendPerGroup}, at::dtype(at::kInt).device(x.device()));
-    int64_t sendCount = sendPerGroup * numLocalExperts * numRanks;
+    auto send_data = torch::empty({num_experts * send_per_group}, at::dtype(at::kInt).device(x.device()));
+    int64_t send_count = send_per_group * num_local_experts * num_ranks;
 
-    auto sendDataOffset = at::empty({numExperts}, at::dtype(at::kInt).device(x.device()));
-    at::Tensor recvData = at::empty({numExperts * sendPerGroup}, at::dtype(at::kInt).device(x.device()));
+    auto send_data_offset = torch::empty({num_experts}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor recv_data = torch::empty({num_experts * send_per_group}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor total_recv_token_ = torch::empty({1}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor recv_count_ = torch::empty({num_experts}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor recv_offset_ = torch::empty({num_experts}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor max_bs_ = torch::empty({1}, at::dtype(at::kInt).device(x.device()));
+    at::Tensor recv_tokens_per_expert_ = torch::empty({num_local_experts}, at::dtype(at::kLong).device(x.device()));
 
-    int64_t localRankSize = numRanks;
-    int64_t localRankId = rank % localRankSize;
+    int64_t local_rank_size = num_ranks;
+    int64_t local_rank_id = rank % local_rank_size;
 
-    EXEC_NPU_CMD(aclnnNotifyDispatch,
-        sendData,
-        numTokensPerExpert, 
-        sendCount,
-        numTokens,
-        groupEpPtr,  // commGroup
-        numRanks,     // rankSize
-        rank,          // rankId
-        localRankSize,
-        localRankId,
-        sendDataOffset,
-        recvData);
+    at::Tensor dispatch_wait_recv_cost_stats_out;
 
-    auto optionsCpu = torch::TensorOptions().dtype(torch::kInt32).device(torch::kCPU);
-    std::vector<int32_t> localExpertAcc(numExperts, 0);
-    auto sendTokenIdxCpu = at::empty({numTokens, numTopk}, optionsCpu);
-    auto sendTokenIdxPtr = sendTokenIdxCpu.data_ptr<int>();
+    EXEC_NPU_CMD(aclnnNotifyDispatch, send_data, num_tokens_per_expert, send_count, num_tokens,
+                 group_ep_ptr, // commGroup
+                 num_ranks,    // rankSize
+                 rank,         // rankId
+                 local_rank_size, local_rank_id, send_data_offset, recv_data, total_recv_token_, recv_count_,
+                 recv_offset_, max_bs_, recv_tokens_per_expert_);
 
-    auto topkIdxCpu = topkIdx.to(at::kCPU);
-    auto topkIdxPtr = topkIdxCpu.data_ptr<int64_t>();
-    for (int i = 0; i < numTokens; ++i) {
-        for (int j = 0; j < numTopk; ++j) {
-            int64_t expertIdx = topkIdxPtr[i * numTopk + j];
-            if (expertIdx >= 0) {
-                int32_t cnt = localExpertAcc[expertIdx];
-                sendTokenIdxPtr[i * numTopk + j] = cnt;
-                localExpertAcc[expertIdx]++;
-            }
-        }
-    }
+    int64_t gBs = max_bs_.item<int>() * num_ranks;
+    int64_t trt = total_recv_token_.item<int>();
+    int num_recv_tokens = (trt == 0) ? 1 : trt;
+    auto expandx_out = use_quant ? torch::empty({num_recv_tokens, hidden}, at::dtype(at::kChar).device(x.device()))
+                                 : torch::empty({num_recv_tokens, hidden}, x.options());
+    auto dynamic_scales_out = torch::empty({num_recv_tokens}, at::dtype(at::kFloat).device(x.device()));
+    auto expand_idx_out = torch::empty({num_recv_tokens * 3}, at::dtype(at::kInt).device(x.device()));
 
-    TORCH_BIND_ASSERT(recvData.dim() == 1 and recvData.is_contiguous());
-    TORCH_BIND_ASSERT(recvData.size(0) % numExperts == 0);
-    at::Tensor recvOffsetCpu = at::empty({numExperts}, optionsCpu);
-    at::Tensor recvCountCpu = at::empty({numExperts}, optionsCpu);
-    auto recvDataCpu = recvData.to(at::kCPU);
-    auto recvDataPtr = recvDataCpu.data_ptr<int>();
-    auto recvCountPtr = recvCountCpu.data_ptr<int>();
-    auto recvOffsetPtr = recvOffsetCpu.data_ptr<int>();
-    int64_t totalRecvTokens = 0;
-    int64_t numMaxDispatchTokensPerRank = 0;
-    std::vector<int64_t> numRecvTokensPerExpertList;
-
-    for (int64_t localE = 0; localE < numLocalExperts; ++localE) {
-        int64_t localExpertRecvTokens = 0;
-        for (int64_t srcRank = 0; srcRank < numRanks; ++srcRank) {
-            int64_t index = localE * numRanks + srcRank;
-            int64_t pairIdx = sendPerGroup * (srcRank * numLocalExperts + localE);
-
-            int recvCnt = recvDataPtr[pairIdx];                 // count from this srcRank for
-                                                                    // this global_expert
-            int recvOff = recvDataPtr[pairIdx + 1];             // offset in that srcRank's window
-            int64_t sendNumTokens = recvDataPtr[pairIdx + 2];  // all bs from rank
-
-            totalRecvTokens += recvCnt;
-            recvCountPtr[index] = totalRecvTokens;
-            recvOffsetPtr[index] = recvOff;
-            numMaxDispatchTokensPerRank = std::max(numMaxDispatchTokensPerRank, sendNumTokens);
-
-            localExpertRecvTokens += recvCnt;
-        }
-        numRecvTokensPerExpertList.push_back(localExpertRecvTokens);
-    }
-    auto option = torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU);
-    at::Tensor numRecvTokensPerExpert = torch::from_blob(
-        numRecvTokensPerExpertList.data(), {static_cast<int64_t>(numRecvTokensPerExpertList.size())}, option)
-        .clone();
-
-    at::Tensor expertIds = topkIdx.to(at::kInt);
-    int64_t tpSize = 1;
-    int64_t tpRank = 0;
-    int64_t quantMode = 0;
-    int64_t globalBs = static_cast<int64_t>(
-        std::max(numMaxDispatchTokensPerRank * numRanks, static_cast<int64_t>(numWorstTokens)));
-
-    auto sendTokenIdx = sendTokenIdxCpu.to(x.device());
-    auto recvOffset = recvOffsetCpu.to(x.device());
-    auto recvCount = recvCountCpu.to(x.device());
-
-    int totalCnt = totalRecvTokens;
-    if (totalCnt == 0) {
-        totalCnt = 1;
-    }
-    auto expandxOut = at::empty({totalCnt, hidden}, x.options());
-    auto dynamicScalesOut = at::empty({totalCnt}, at::dtype(at::kFloat).device(x.device()));
-    auto expandIdxOut = at::empty({totalCnt * 3}, at::dtype(at::kInt).device(x.device()));
-
-    EXEC_NPU_CMD(aclnnMoeDispatchNormal,
-        newX,
-        expertIds,
-        sendDataOffset,
-        sendTokenIdx,
-        recvOffset,
-        recvCount,
-        groupEpPtr,  // commGroup
-        numRanks,     // rankSize
-        rank,          // rankId
-        groupEpPtr,
-        tpSize,
-        tpRank,
-        numExperts,
-        quantMode,
-        globalBs,
-        expandxOut,
-        dynamicScalesOut,
-        expandIdxOut);
+    EXEC_NPU_CMD(aclnnMoeDispatchNormal, x, expert_ids, send_data_offset, send_token_idx_small, recv_offset_,
+                 recv_count_, group_ep_ptr,
+                 num_ranks, // rankSize
+                 rank,      // rankId
+                 group_ep_ptr, tp_size, tp_rank, num_experts, quant_mode, gBs, expandx_out, dynamic_scales_out,
+                 expand_idx_out, dispatch_wait_recv_cost_stats_out);
 
     // Return values
-    return {expandxOut, expandIdxOut, recvCount, numRecvTokensPerExpert};
+    return {expandx_out, dynamic_scales_out, expand_idx_out, recv_count_, recv_tokens_per_expert_};
 }
 
 tensor_list MoeDispatchPrefillBackwardImplNpu(const at::Tensor &self)
 {
     at::Tensor result = at::Tensor(self); // 创建输出内存
-    return {result, result, result, result};
+    return {result, result, result, result, result};
 }
 
 /* Normal类算子形状无法提前推导，不支持meta设备注册不支持GE使用 */
-std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> MoeDispatchPrefillImpl(
-    const at::Tensor& x,
-    const at::Tensor& topkIdx,
-    const at::Tensor& topkWeights,
-    const at::Tensor& numTokensPerRank,
-    const at::Tensor& isTokenInRank,
-    at::Tensor& numTokensPerExpert,
-    int64_t numWorstTokens,
-    c10::string_view groupEp,
-    int64_t rank,
-    int64_t numRanks)
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
+MoeDispatchPrefillImpl(const at::Tensor &x, const at::Tensor &topk_idx, const at::Tensor &topk_weights,
+                       const at::Tensor &num_tokens_per_expert, const at::Tensor &send_token_idx_small,
+                       c10::string_view groupEp, int64_t rank, int64_t num_ranks, bool use_quant)
 {
     static auto op = torch::Dispatcher::singleton()
-                        .findSchemaOrThrow("umdk_cam_op_lib::moe_dispatch_prefill", "")
-                        .typed<decltype(MoeDispatchPrefillImpl)>();
-    return op.call(x, topkIdx, topkWeights, numTokensPerRank, isTokenInRank, \
-        numTokensPerExpert, numWorstTokens, groupEp, rank, numRanks);
+                         .findSchemaOrThrow("umdk_cam_op_lib::moe_dispatch_prefill", "")
+                         .typed<decltype(MoeDispatchPrefillImpl)>();
+    return op.call(x, topk_idx, topk_weights, num_tokens_per_expert, send_token_idx_small, groupEp, rank, num_ranks,
+                   use_quant);
 }
 
 // 通过继承torch::autograd::Function类实现前反向绑定
 class ExtMoeDispatchPrefill : public torch::autograd::Function<ExtMoeDispatchPrefill> {
-public:
-    static tensor_list forward(
-        AutogradContext *ctx, \
-        const at::Tensor& x,
-        const at::Tensor& topkIdx,
-        const at::Tensor& topkWeights,
-        const at::Tensor& numTokensPerRank,
-        const at::Tensor& isTokenInRank,
-        at::Tensor& numTokensPerExpert,
-        int64_t numWorstTokens,
-        c10::string_view groupEp,
-        int64_t rank,
-        int64_t numRanks)
+  public:
+    static tensor_list forward(AutogradContext *ctx, const at::Tensor &x, const at::Tensor &topk_idx,
+                               const at::Tensor &topk_weights, const at::Tensor &num_tokens_per_expert,
+                               const at::Tensor &send_token_idx_small, c10::string_view groupEp, int64_t rank,
+                               int64_t num_ranks, bool use_quant)
     {
         at::AutoDispatchBelowADInplaceOrView guard;
-        auto result = MoeDispatchPrefillImpl(x, topkIdx, topkWeights, numTokensPerRank, \
-            isTokenInRank, numTokensPerExpert, numWorstTokens, groupEp, rank, numRanks);
+        auto result = MoeDispatchPrefillImpl(x, topk_idx, topk_weights, num_tokens_per_expert, send_token_idx_small,
+                                             groupEp, rank, num_ranks, use_quant);
 
-        return {std::get<0>(result), std::get<1>(result), std::get<2>(result), std::get<3>(result)};
+        return {std::get<0>(result), std::get<1>(result), std::get<2>(result), std::get<3>(result),
+                std::get<4>(result)};
     }
 
-    static tensor_list backward(
-        AutogradContext *ctx, \
-        tensor_list grad_outputs)
+    static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs)
     {
-        return {at::Tensor(), at::Tensor(), at::Tensor(), at::Tensor()};
+        return {at::Tensor(), at::Tensor(), at::Tensor(), at::Tensor(), at::Tensor()};
     }
 };
 
-std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor> MoeDispatchPrefillImplAutograd(
-    const at::Tensor& x,
-    const at::Tensor& topkIdx,
-    const at::Tensor& topkWeights,
-    const at::Tensor& numTokensPerRank,
-    const at::Tensor& isTokenInRank,
-    at::Tensor& numTokensPerExpert,
-    int64_t numWorstTokens,
-    c10::string_view groupEp,
-    int64_t rank,
-    int64_t numRanks)
+std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor, at::Tensor>
+MoeDispatchPrefillImplAutograd(const at::Tensor &x, const at::Tensor &topk_idx, const at::Tensor &topk_weights,
+                               const at::Tensor &num_tokens_per_expert, const at::Tensor &send_token_idx_small,
+                               c10::string_view groupEp, int64_t rank, int64_t num_ranks, bool use_quant)
 {
-    auto result = ExtMoeDispatchPrefill::apply(x, topkIdx, topkWeights, numTokensPerRank, \
-        isTokenInRank, numTokensPerExpert, numWorstTokens, groupEp, rank, numRanks);
-    return std::make_tuple(result[0], result[1], result[2], result[3]);
+    auto result = ExtMoeDispatchPrefill::apply(x, topk_idx, topk_weights, num_tokens_per_expert, send_token_idx_small,
+                                               groupEp, rank, num_ranks, use_quant);
+    return std::make_tuple(result[0], result[1], result[2], result[3], result[4]);
 }
 
 // moe_dispatch_prefill
