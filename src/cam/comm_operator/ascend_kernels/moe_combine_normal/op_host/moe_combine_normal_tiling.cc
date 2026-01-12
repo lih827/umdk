a233bee37cfcdf59cb3b50d05a58b9415e77239f
lih827 <383084552@qq.com>
2026-01-12 11:31:26 +0800

    fix

--- src/cam/comm_operator/ascend_kernels/moe_combine_normal/op_host/moe_combine_normal_tiling.cc
+++ src/cam/comm_operator/ascend_kernels/moe_combine_normal/op_host/moe_combine_normal_tiling.cc
@@ -25,9 +25,9 @@
 #include "../op_kernel/moe_combine_normal_tiling.h"
 #include "error_log.h"
 #include "graph/utils/type_utils.h"
-#include "mc2_tiling_utils.h"
 #include "register/op_def_registry.h"
 #include "register/tilingdata_base.h"
+#include "tiling/mc2_tiling_utils.h"
 #include "tiling/tiling_api.h"
 #include "tiling_args.h"
 
@@ -35,6 +35,29 @@ using namespace AscendC;
 using namespace ge;
 
 namespace {
+class Mc2TilingUtils {
+  public:
+#define HCCL_BUFFSIZE "HCCL_BUFFSIZE"
+    static uint64_t GetMaxWindowSize()
+    {
+        uint16_t defaultWindowSize = 200;
+        if (getenv(HCCL_BUFFSIZE) == nullptr) {
+            OP_LOGD("", "Env HCCL_BUFFSIZE don't set");
+        } else {
+            try {
+                std::string envStr(getenv(HCCL_BUFFSIZE));
+                defaultWindowSize = std::stoi(envStr);
+            } catch (const std::invalid_argument &ia) {
+                OP_LOGE("", "Invalid argument when parsing HCCL_BUFFSIZE: %s", ia.what());
+            } catch (const std::out_of_range &oor) {
+                OP_LOGE("", "Out of range when parsing HCCL_BUFFSIZE: %s", oor.what());
+            }
+        }
+        const uint64_t maxWindowSize = static_cast<uint64_t>(defaultWindowSize) * 1024UL * 1024UL;
+        OP_LOGI("", "Get maxWindowSize is %lu", maxWindowSize);
+        return maxWindowSize;
+    }
+};
 using Moe::COMBINE_STATE_WIN_OFFSET;
 using Moe::NOTIFY_DISPATCH_WIN_OFFSET;
 constexpr uint32_t RECV_X_INDEX = 0;
